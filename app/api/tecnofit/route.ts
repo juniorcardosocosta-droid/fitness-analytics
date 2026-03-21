04438bdbb69d93d25a0f89fecf9c71b53d329c8c
Junior Costa <juniorcardosocosta@gmail.com>
2026-03-21 09:02:57 -0300

    teste endpoint real tecnofit

--- app/api/tecnofit/route.ts
+++ app/api/tecnofit/route.ts
@@ -53,7 +53,7 @@ export async function GET() {
    const token = loginData.token
 
     // 📊 Buscar alunos
-    const apiResponse = await fetch("https://integracao.tecnofit.com.br/v1/customer", {
+    const apiResponse = await fetch("https://integracao.tecnofit.com.br/v1/products/1", {
       method: "GET",
       headers: {
         Authorization: `Bearer ${token}`,
